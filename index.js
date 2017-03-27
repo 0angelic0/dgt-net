02f984c224987b7f51a371ebe6bbf8b2caf25722
0angelic0 <pisit@digitopolisstudio.com>
2017-03-27 22:47:43 +0700

    Export packet_reader and packet_writer.

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 
 let server = require('./src/server/server')
 let client = require('./src/client/client')
+let packet_reader = require('./src/packet_reader')
+let packet_writer = require('./src/packet_writer')
 
 module.exports.server = server
 module.exports.client = client
+module.exports.packet_reader = packet_reader
+module.exports.packet_writer = packet_writer
